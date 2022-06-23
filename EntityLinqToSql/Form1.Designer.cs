9c8c0cd68122974f00eae814090e35e18b244be1
merve_engineer <merve_engineer@hotmail.com>
2022-06-23 03:44:30 +0300

    Proje dosyası ekle.

---
+++ EntityLinqToSql/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace EntityLinqToSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
+            this.cmbTedarikci = new System.Windows.Forms.ComboBox();
+            this.numFiyat = new System.Windows.Forms.NumericUpDown();
+            this.numStok = new System.Windows.Forms.NumericUpDown();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.rdbUrunAdi = new System.Windows.Forms.RadioButton();
+            this.rdbFiyat = new System.Windows.Forms.RadioButton();
+            this.rdbStok = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFiyat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStok)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 142);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1008, 408);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ürün Adı:";
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(12, 69);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(100, 22);
+            this.txtUrunAdi.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kategori:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tedarikçi:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(440, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Fiyat:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(602, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Stok:";
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Location = new System.Drawing.Point(134, 67);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(121, 24);
+            this.cmbKategori.TabIndex = 7;
+            // 
+            // cmbTedarikci
+            // 
+            this.cmbTedarikci.FormattingEnabled = true;
+            this.cmbTedarikci.Location = new System.Drawing.Point(277, 69);
+            this.cmbTedarikci.Name = "cmbTedarikci";
+            this.cmbTedarikci.Size = new System.Drawing.Size(121, 24);
+            this.cmbTedarikci.TabIndex = 8;
+            // 
+            // numFiyat
+            // 
+            this.numFiyat.Location = new System.Drawing.Point(429, 70);
+            this.numFiyat.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numFiyat.Name = "numFiyat";
+            this.numFiyat.Size = new System.Drawing.Size(120, 22);
+            this.numFiyat.TabIndex = 9;
+            // 
+            // numStok
+            // 
+            this.numStok.Location = new System.Drawing.Point(581, 71);
+            this.numStok.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numStok.Name = "numStok";
+            this.numStok.Size = new System.Drawing.Size(120, 22);
+            this.numStok.TabIndex = 10;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(723, 71);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 11;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(815, 73);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 22);
+            this.btnSil.TabIndex = 12;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(907, 72);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(97, 23);
+            this.btnGuncelle.TabIndex = 13;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 576);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 576);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Filtre";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Hizala";
+            // 
+            // rdbUrunAdi
+            // 
+            this.rdbUrunAdi.AutoSize = true;
+            this.rdbUrunAdi.Location = new System.Drawing.Point(94, 115);
+            this.rdbUrunAdi.Name = "rdbUrunAdi";
+            this.rdbUrunAdi.Size = new System.Drawing.Size(84, 21);
+            this.rdbUrunAdi.TabIndex = 17;
+            this.rdbUrunAdi.TabStop = true;
+            this.rdbUrunAdi.Text = "Ürün Adı";
+            this.rdbUrunAdi.UseVisualStyleBackColor = true;
+            this.rdbUrunAdi.CheckedChanged += new System.EventHandler(this.rdbUrunAdi_CheckedChanged);
+            // 
+            // rdbFiyat
+            // 
+            this.rdbFiyat.AutoSize = true;
+            this.rdbFiyat.Location = new System.Drawing.Point(211, 113);
+            this.rdbFiyat.Name = "rdbFiyat";
+            this.rdbFiyat.Size = new System.Drawing.Size(59, 21);
+            this.rdbFiyat.TabIndex = 18;
+            this.rdbFiyat.TabStop = true;
+            this.rdbFiyat.Text = "Fiyat";
+            this.rdbFiyat.UseVisualStyleBackColor = true;
+            // 
+            // rdbStok
+            // 
+            this.rdbStok.AutoSize = true;
+            this.rdbStok.Location = new System.Drawing.Point(304, 115);
+            this.rdbStok.Name = "rdbStok";
+            this.rdbStok.Size = new System.Drawing.Size(57, 21);
+            this.rdbStok.TabIndex = 19;
+            this.rdbStok.TabStop = true;
+            this.rdbStok.Text = "Stok";
+            this.rdbStok.UseVisualStyleBackColor = true;
+            this.rdbStok.CheckedChanged += new System.EventHandler(this.rdbUrunAdi_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(211, 113);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 21);
+            this.radioButton1.TabIndex = 18;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Fiyat";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.rdbUrunAdi_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 610);
+            this.Controls.Add(this.rdbStok);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.rdbFiyat);
+            this.Controls.Add(this.rdbUrunAdi);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.numStok);
+            this.Controls.Add(this.numFiyat);
+            this.Controls.Add(this.cmbTedarikci);
+            this.Controls.Add(this.cmbKategori);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUrunAdi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numFiyat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numStok)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbKategori;
+        private System.Windows.Forms.ComboBox cmbTedarikci;
+        private System.Windows.Forms.NumericUpDown numFiyat;
+        private System.Windows.Forms.NumericUpDown numStok;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton rdbUrunAdi;
+        private System.Windows.Forms.RadioButton rdbFiyat;
+        private System.Windows.Forms.RadioButton rdbStok;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ EntityLinqToSql/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityLinqToSql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //urunler tablosundaki verileri listelettik!!.Entity framework kendi entity ve facade oluşturdu
+            //otomatik oluşturduğu get ile select komutu oluşturur ve listeleme işlemini yapar.
+            KuzeyYeliDataContext ctx = new KuzeyYeliDataContext();
+            dataGridView1.DataSource = ctx.Urunlers;
+
+            //////////////
+            cmbKategori.DataSource = ctx.Kategorilers;
+            cmbKategori.DisplayMember = "KategoriAdi";//görünmesini istedğimiz property seçtik
+            cmbKategori.ValueMember = "KategoriID"; //selectedvalue da gösterilmesini istediğimiz değerdir
+            cmbTedarikci.DataSource = ctx.Tedarikcilers;
+
+            cmbTedarikci.DisplayMember = "SirketAdi";
+            cmbTedarikci.ValueMember = "TedarikciID";
+         
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            KuzeyYeliDataContext ctx = new KuzeyYeliDataContext();
+            Urunler urn = new Urunler();
+            urn.UrunAdi = txtUrunAdi.Text;
+            urn.Fiyat = numFiyat.Value;
+            urn.Stok = (short)numStok.Value;
+            urn.KategoriID = (int)cmbKategori.SelectedValue; //CAST İŞLEMİ
+            urn.TedarikciID = (int)cmbTedarikci.SelectedValue;
+
+            ctx.Urunlers.InsertOnSubmit(urn);
+            ctx.SubmitChanges();//komutu olmadan ekleme,silme vb işlemler database de gerçekleşmez,işlem bekler
+            MessageBox.Show("Ürün Eklendi");
+            dataGridView1.DataSource = ctx.Urunlers;//güncel listeyi yazdırdık
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            //datagriedview selection mode--full row select yap!!
+
+            //önce seçili satırda urunun ıd alıp sonra silme işlemi yapılır!
+            int urunid = (int)dataGridView1.CurrentRow.Cells["UrunId"].Value;
+            KuzeyYeliDataContext ctx = new KuzeyYeliDataContext();
+            Urunler urn=ctx.Urunlers.SingleOrDefault(urun =>urun.UrunID==urunid);//lambda expressions şart verip ürün seçilir
+           //=>urun öyle ki....
+            
+            //singleordefault metotu contextin oluşturduğu sorguda ilk kolonu alır...
+            //lambda expressions id almak için kullanılır.Sorgudaki ilk elemanı getirir.(row 0.elemanı alır)
+            //Birden fazla eleman gelirse  eleman yoksa hata verir.Eleman yoksa default sayesinde null döner hata oluşmaz!!
+
+            ctx.Urunlers.DeleteOnSubmit(urn);
+            ctx.SubmitChanges();
+            MessageBox.Show("Ürün Silindi");
+            dataGridView1.DataSource = ctx.Urunlers;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = dataGridView1.CurrentRow;
+            txtUrunAdi.Text = row.Cells["UrunAdi"].Value.ToString();//object tanımlama olduğundan tür dönüşümü(cast) itiyor!!
+            txtUrunAdi.Tag = row.Cells["UrunId"].Value;
+            //urunid değerini txtboxun tag tutmasını sağlıyoruz
+            //yanlışlıkla datagridview başka ürün seçilir güncelleme yapılırsa önceki bilgiler yerine yeni ürün bilgileri güncellenir
+            //işte bunu önlemek için tag ıd tutulur!!
+            cmbKategori.SelectedValue = row.Cells["KategoriID"].Value;
+            cmbTedarikci.SelectedValue = row.Cells["TedarikciID"].Value;
+            numFiyat.Value = (decimal)row.Cells["Fiyat"].Value;
+            numStok.Value = (short)row.Cells["Stok"].Value;
+
+
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int urunid = (int)txtUrunAdi.Tag;
+            KuzeyYeliDataContext ctx = new KuzeyYeliDataContext();
+            Urunler urn = ctx.Urunlers.SingleOrDefault(urun => urun.UrunID == urunid);
+            urn.UrunAdi= txtUrunAdi.Text;
+            urn.Fiyat = numFiyat.Value;
+            urn.Stok =(short) numStok.Value;
+            urn.TedarikciID = (int)cmbTedarikci.SelectedValue;
+            urn.KategoriID = (int)cmbKategori.SelectedValue;
+            ctx.SubmitChanges();
+            MessageBox.Show("Ürün Güncellendi");
+            dataGridView1.DataSource = ctx.Urunlers;
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            KuzeyYeliDataContext ctx = new KuzeyYeliDataContext();
+            dataGridView1.DataSource = ctx.Urunlers.Where(x => x.UrunAdi.Contains(textBox1.Text));
+        }
+
+        private void rdbUrunAdi_CheckedChanged(object sender, EventArgs e)
+        {
+            KuzeyYeliDataContext ctx = new KuzeyYeliDataContext();
+            if (rdbUrunAdi.Checked)
+                dataGridView1.DataSource = ctx.Urunlers.OrderBy(x => x.UrunAdi);
+            //x-urun nesnesi
+            else if (radioButton1.Checked)
+                dataGridView1.DataSource = ctx.Urunlers.OrderByDescending(x => x.Fiyat);//azalan sırada listeleme yaptık
+            else if (rdbStok.Checked)
+                dataGridView1.DataSource = ctx.Urunlers.OrderByDescending(x => x.Stok);
+        }
+    }
+}
